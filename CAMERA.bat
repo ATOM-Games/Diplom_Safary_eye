a2b16f8753944af55f5cd3d668a75b464c042aa3
ABC-AORUS\silat <silatrotila0atom@gmail.com>
2023-07-01 11:42:03 +0400

    Diplom _ v1.0.0.0 _ [ ' diplom all ' ]

---
+++ CAMERA.bat
@@ -0,0 +1,3 @@
+start http://10.0.0.179:10001/admin
+cd C:\Users\1\PycharmProjects\cam\Retranslator
+..\py\python.exe retranslator.py
---
+++ FRONT.bat
@@ -0,0 +1,3 @@
+start http://localhost:4200/
+cd C:\Users\1\PycharmProjects\Diplom
+npm run dev
